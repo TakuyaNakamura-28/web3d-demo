07f60c1177f7b54a0e8b59a1e90af41dd65433e0
Kaylee Kobayashi <kaylee@bekindlabs.com>
2025-05-30 16:44:21 +0900

    further csv loading refactor for readability.

--- demo/src/forcePlates.ts
+++ demo/src/forcePlates.ts
@@ -12,41 +12,18 @@ export function addForcePlateGridToScene(scene: Scene, start: number, spacing: n
     })
 }
 
-export type ForcePlateDatum  = { x: number, y: number, z: number, t: number, px: number, py: number }
+export type ForcePlateDatum = { x: number, y: number, z: number, t: number, px: number, py: number }
 
-export function parseForcePlateData (csvText: string) : Array<Array<ForcePlateDatum>> {
-    return csvText.split('\n').map(row => row.split(',')).filter(row => row.length > 1).map(row => [
-        {
-            x: Number(row[6 - 1]),
-            y: Number(row[6]),
-            z: Number(row[6 - 2]),
-            t: Number(row[6 + 3]),
-            px: Number(row[6 + 4]),
-            py: Number(row[6 + 5])
-        },
-        {
-            x: Number(row[39 - 1]),
-            y: Number(row[39]),
-            z: Number(row[39 - 2]),
-            t: Number(row[39 + 3]),
-            px: Number(row[39 + 4]),
-            py: Number(row[39 + 5])
-        },
-        {
-            x: Number(row[17 - 1]),
-            y: Number(row[17]),
-            z: Number(row[17 - 2]),
-            t: Number(row[17 + 3]),
-            px: Number(row[17 + 4]),
-            py: Number(row[17 + 5])
-        },
-        {
-            x: Number(row[28 - 1]),
-            y: Number(row[28]),
-            z: Number(row[28 - 2]),
-            t: Number(row[28 + 3]),
-            px: Number(row[28 + 4]),
-            py: Number(row[28 + 5])
-        },
-    ])
+export function parseForcePlateData(csvText: string): Array<Array<ForcePlateDatum>> {
+    const csvPlateDataOffsets = [4, 37, 15, 26]
+    return csvText.split('\n').map(row => row.split(',')).filter(row => row.length > 1).map(row =>
+        csvPlateDataOffsets.map((offset) => ({
+            z: Number(row[offset]),
+            x: Number(row[offset + 1]),
+            y: Number(row[offset + 2]),
+            t: Number(row[offset + 5]),
+            px: Number(row[offset + 6]),
+            py: Number(row[offset + 7])
+        }))
+    )
 }
